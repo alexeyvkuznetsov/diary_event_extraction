17d4dda313cdf8143bfad0587581b13e94e65aff
Alexey Kuznetsov <57808789+alexeyvkuznetsov@users.noreply.github.com>
2025-08-06 14:57:32 +0300

    2

--- cody.py
+++ cody.py
@@ -1,3 +1,23 @@
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+#API_KEY = os.getenv("CODY_APY_KEY")
+print(OpenAI(base_url="https://cody.su/api/v1", api_key="cody-NPyRFCZ7Pm5pNjIuwMb655VvAGopee0w1FIxqmA0b4GElHEOfU6uU8PhNnUbPn4GQMUYC63Wwwf4-qjjN9KWAQ").models.list())
+
+
+from openai import OpenAI
+
+client = OpenAI(base_url="https://cody.su/api/v1", api_key="cody-NPyRFCZ7Pm5pNjIuwMb655VvAGopee0w1FIxqmA0b4GElHEOfU6uU8PhNnUbPn4GQMUYC63Wwwf4-qjjN9KWAQ")
+
+resp = client.chat.completions.create(
+    model="gpt-4.1",
+    messages=[{"role": "user", "content": "Короткая история про котёнка"}],
+)
+print(resp.choices[0].message.content)
+
+
 from openai import OpenAI
 
 client = OpenAI(
@@ -7,12 +27,9 @@ client = OpenAI(
     api_key="cody-NPyRFCZ7Pm5pNjIuwMb655VvAGopee0w1FIxqmA0b4GElHEOfU6uU8PhNnUbPn4GQMUYC63Wwwf4-qjjN9KWAQ",
     )
 
-
-
-
 completion = client.chat.completions.create(
 
-    model="grok-3",
+    model="gpt-4.1",
 
     messages=[
 
@@ -22,11 +39,4 @@ completion = client.chat.completions.create(
     )
 
 
-
-
 print(completion.choices[0].message.content)
-
-
-
-from openai import OpenAI
-print(OpenAI(base_url="https://cody.su/api/v1", api_key="cody-NPyRFCZ7Pm5pNjIuwMb655VvAGopee0w1FIxqmA0b4GElHEOfU6uU8PhNnUbPn4GQMUYC63Wwwf4-qjjN9KWA").models.list()
